db0735f15ec2a0de9197c7477d4bbbf1d62ef994
elauser <1810653024@stud.fh-kufstein.ac.at>
2018-11-29 14:56:05 +0100

    Aufgabe 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
+        double taschenrechnerA = 10;
+        double taschenrechnerB = 20;
+        int tIntA = 10;
+        int tIntB = 20;
+        System.out.println(Taschenrechner.add(taschenrechnerA, taschenrechnerB));
+        System.out.println(Taschenrechner.sub(taschenrechnerA, taschenrechnerB));
+        System.out.println(Taschenrechner.div(taschenrechnerA, taschenrechnerB));
+        System.out.println(Taschenrechner.mul(taschenrechnerA, taschenrechnerB));
+
+        System.out.println(Taschenrechner.add(tIntA, tIntB));
+        System.out.println(Taschenrechner.sub(tIntA, tIntB));
+        System.out.println(Taschenrechner.div(tIntA, tIntB));
+        System.out.println(Taschenrechner.mul(tIntA, tIntB));
+
         partialString(JOptionPane.showInputDialog(null, "Geben Sie eine Zahl ein di mindestens 3 Zeichen lang ist"));
     }
     private static void partialString(String a){
---
+++ src/Taschenrechner.java
@@ -0,0 +1,26 @@
+public class Taschenrechner {
+    public static double add(double a, double b){
+        return (a+b);
+    }
+    public static double sub(double a, double b){
+        return (a-b);
+    }
+    public static double div(double a, double b){
+        return (a/b);
+    }
+    public static double mul(double a, double b){
+        return (a*b);
+    }
+    public static int add(int a, int b){
+        return (a+b);
+    }
+    public static int sub(int a, int b){
+        return (a-b);
+    }
+    public static int div(int a, int b){
+        return (a/b);
+    }
+    public static int mul(int a, int b){
+        return (a*b);
+    }
+}
